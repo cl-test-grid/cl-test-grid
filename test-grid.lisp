f2e229491c776219d0b24e1b60c3930c92ff359f
Daniel Zaru <danielzaru@gmail.com>
2011-12-09 09:07:15 -0800

    added function format-results

--- test-grid.lisp
+++ test-grid.lisp
@@ -950,13 +950,11 @@ to the cl-test-grid issue tracker:
 ;; until we move the reporting to a separate
 ;; asdf system, we don't want the dependency 
 ;; on blobstore here.
-(defun format-results (lib-result)
-  (let ((links))
-    (dolist (value lib-result)
-      (setf links (concatenate 'string links " " (format nil "<a href=\"~a\">~a</a>" 
-                                                         (lib-log-uri nil value)
-                                                         (string-downcase (getf value :status))))))
-    links))
+(defun format-results (out lib-result)
+  (dolist (value lib-result)
+    (format out "<a href=\"~a\">~a</a> " 
+            (lib-log-uri nil value)
+            (string-downcase (getf value :status)))))
 
 (defun blob-uri (blob-key)
   (format nil "~A/blob?key=~A" 
@@ -1237,11 +1235,12 @@ as a parameter"
         (dolist (col cols)
           (funcall index-key-setter index-key row col)
           (let ((data (gethash index-key joined-index)))
-            (format out "<td>~a</td>" (get-link data))))
+            (with-output-to-string (out) 
+              (format-results out data))
+            (princ "<td>" out) (format-results out data) (princ "</td>" out)))
         (format out "</tr>~%"))))
   (princ "</table>" out))
   
-
 (defun print-pivot-reports (db)
   (let ((joined-index (build-joined-index db))
         (reports-dir (reports-dir)))
