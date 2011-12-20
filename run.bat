485f7bfa71da28c14d0573d84ccaca14195a3d52
Anton Vodonosov <avodonosov@yandex.ru>
2011-12-20 04:17:35 +0300

    Added the run.bat to record the command line parameters of the CL implementations I use to load the agent.lisp

---
+++ run.bat
@@ -0,0 +1,5 @@
+rem C:\Users\anton\unpacked\abcl\abcl.bat --load agent.lisp
+
+rem "C:\Program Files (x86)\acl82express\alisp.exe" -L C:\Users\anton\quicklisp\setup.lisp -L agent.lisp
+
+C:\Users\anton\unpacked\ecl\ecl-11.1.1\bin\ecl -load agent.lisp
