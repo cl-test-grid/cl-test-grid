e08444d52b6cf14f179df676a179cbd790e5a4da
Anton Vodonosov <avodonosov@yandex.ru>
2011-12-29 06:11:21 +0300

    Added the CCL invocation with command line parameters to the run.bat

--- run.bat
+++ run.bat
@@ -2,4 +2,8 @@ rem C:\Users\anton\unpacked\abcl\abcl.bat --load agent.lisp
 
 rem "C:\Program Files (x86)\acl82express\alisp.exe" -L C:\Users\anton\quicklisp\setup.lisp -L agent.lisp
 
-C:\Users\anton\unpacked\ecl\ecl-11.1.1\bin\ecl -load agent.lisp
+rem C:\Users\anton\unpacked\ecl\ecl-11.1.1\bin\ecl -load agent.lisp
+
+rem C:\Users\anton\unpacked\ccl\ccl-1.6-windowsx86\wx86cl64.exe --load agent.lisp --eval "(quit)"
+rem C:\Users\anton\unpacked\ccl\ccl-1.7-windows\wx86cl64.exe --load agent.lisp --eval "(quit)"
+C:\Users\anton\unpacked\ccl\ccl-1.7-windows\wx86cl.exe --quiet --load agent.lisp --eval "(quit)"
