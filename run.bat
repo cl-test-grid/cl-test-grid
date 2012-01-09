9b983d1bdd53ca2e1b8252dc36430e4d8a7c7461
Anton Vodonosov <avodonosov@yandex.ru>
2012-01-09 17:38:21 +0300

    Updated ECL parameters in run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-C:\Users\anton\unpacked\ccl\ccl-1.7-windows\wx86cl64.exe --no-init --load ensure-quicklisp-updated.lisp --eval "(quit)"
+rem C:\Users\anton\unpacked\ccl\ccl-1.7-windows\wx86cl64.exe --no-init --load ensure-quicklisp-updated.lisp --eval "(quit)"
 
 rem C:\Users\anton\unpacked\ccl\ccl-1.7-windows\wx86cl64.exe --no-init --load quicklisp/setup.lisp --load run-agent.lisp --eval "(quit)"
 
@@ -8,7 +8,7 @@ rem C:\Users\anton\unpacked\abcl\abcl.bat --load agent.lisp
 
 rem "C:\Program Files (x86)\acl82express\alisp.exe" -qq -L quicklisp\setup.lisp -L run-agent.lisp
 
-rem C:\Users\anton\unpacked\ecl\ecl-11.1.1\bin\ecl -load agent.lisp
+C:\Users\anton\unpacked\ecl\ecl-11.1.1\bin\ecl -norc -load quicklisp\setup.lisp -load run-agent.lisp -eval "(quit)"
 
 rem C:\Users\anton\unpacked\ccl\ccl-1.6-windowsx86\wx86cl64.exe --load run-agent.lisp --eval "(quit)"
 rem C:\Users\anton\unpacked\ccl\ccl-1.7-windows\wx86cl64.exe --load run-agent.lisp --eval "(quit)"
