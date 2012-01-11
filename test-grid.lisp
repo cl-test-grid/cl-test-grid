759a7fa1b49845880578c854a2f8d70d30b24eb4
Anton Vodonosov <avodonosov@yandex.ru>
2012-01-11 08:37:53 +0300

    Uncomented ECL hardcoded failure for alexandria tests because we are going to test the ECL 11.1.1 release where the stack overflow bug exists.

--- test-grid.lisp
+++ test-grid.lisp
@@ -46,14 +46,15 @@ contains the tests of _both_ libraries."
   
 (defmethod libtest ((library-name (eql :alexandria)))
 
-; commented out because in the recent version of ECL it is not needed.
-; we keep it here in case we want to test the 11.1.1 release
-; (until new release is out).
-;
-;  #+ecl
-;  (progn
-;    (format t "ECL 11.1.1 has bug causing a stack overflow on alexandria tests. http://sourceforge.net/tracker/?func=detail&aid=3463131&group_id=30035&atid=398053~%")
-;    (return-from libtest :fail))
+; We keep the below hardcoded failure in case we want to test 
+; the ECL 11.1.1 release (until new release is out).
+; It is commented out or uncommented depending on what
+; ECL release we are goint to test.
+
+  #+ecl
+  (progn
+    (format t "ECL 11.1.1 has bug causing a stack overflow on alexandria tests. http://sourceforge.net/tracker/?func=detail&aid=3463131&group_id=30035&atid=398053~%")
+    (return-from libtest :fail))
 
   ;; The test framework used: rt.
   (clean-rt)
