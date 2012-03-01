1ba0fa1c33842f9a32a7e689df6ba8ef94f6cc45
Anton Vodonosov <avodonosov@yandex.ru>
2012-03-01 05:01:42 +0300

    Clarify and comment the pivot reports code.

--- test-grid.lisp
+++ test-grid.lisp
@@ -1139,6 +1139,34 @@ values: :OK, :UNEXPECTED-OK, :FAIL, :NO-RESOURSE, :KNOWN-FAIL."
 
 ;; ========= Pivot Reports ==================
 
+;; Conceptualy, test results are objects with various fields:
+;; :libname, :lib-world, :lisp, :log-byte-size, :test-duration,
+;; :log-byte-length, etc.
+;;
+;; To build pivot reports, we need to access the
+;; results for the report table cells. Every table
+;; cell is lying on the crossing of the table rows
+;; and columns.
+;;
+;; In other words, we need to access test results for every
+;; combination of values for the fields we put into rows
+;; and columsn headers.
+;;
+;; Currently we want only 3 properties in the row or column
+;; headers: :lib-world, :lisp, :libname.
+;;
+;; Let's build the index - a hash table where keys are
+;; 3 element lists representing a particular combination
+;; of lib-world, libname and lisp; and the hash table value is
+;; a list of test results for that combination:
+;;
+;; ("sbcl-win-1" "quicklisp 1" "alexandria")     -> (<test results>)
+;; ("sbcl-win-1" "quicklisp 1" "babel")          -> (<test results>)
+;; ("sbcl-linux-1" "quicklisp 1" "alexandria")   -> (<test results>)
+;; ("sbcl-linux-1" "quicklisp 2" "alexandria")   -> (<test results>)
+;; ("clisp-win-1" "quicklisp 2" "flexi-streams") -> (<test results>)
+;; ...
+;;
 (defun do-results-impl (db handler)
   "Handler is a function of two arguments: TEST-RUN and LIB-RESULT"
     (dolist (test-run (getf db :runs))
@@ -1161,56 +1189,26 @@ values: :OK, :UNEXPECTED-OK, :FAIL, :NO-RESOURSE, :KNOWN-FAIL."
               (gethash (list lisp lib-world libname) all-results))))
     all-results))
 
-#|
-HTML table properties:
- - rows and cols are not equal: html tables are row-first - TR includes TDs
+;; The pivot reports code below does not know exact form
+;; of the index key - in what order lib-world, lisp and libname
+;; values are specified in the key. Moreover, the pivot reports code
+;; does not know we choses only these 3 properties for the pivot table
+;; headers - it receives the property names for row and column headers
+;; as parameters. All that the pivot code below knows, is that the
+;; index is a hash table where keys store field values somehow,
+;; and that the hash tabble values are lists of results.
+;;
+;; To abstract away the index format we provide the following functions:
 
-Only the deepest level row and col fields will have corresponding
-TR and TD cells in the table data (we do not consider the table
-column headers now).
-
-And the TD cells are included in a TR.
-
-Algorithm sketch:
-  Iterate over all row properties and their values in rows.
-  For every deepest level combination create a TR (if
-  any values exist for this combination).
-
-    Iterate over all the col properties and their values.
-    For every deepest level combination create a TD.
-
-partitioning structure
------------------------------
-    field1 | field2
------------------------------
-    field1 val1 (count)
-             field2 val1 (count)
-             field2 val2 (count)
-    field1 val2 (count)
-             field2 val1 (count)
-             field2 val3 (count)
-    field1 val3 (count)
-             field2 val2 (count)
-
-
-<tr> <td> <td> <td>
-<tr>      <td> <td>
-<tr>           <td>
-<tr>      <td> <td>
-<tr>      <td> <td>
-<tr>           <td>
-<tr> <td> <td> <td>
-<tr>      <td> <td>
-<tr> <td> <td> <td>
-<tr>      <td> <td>
-<tr>           <td>
-
-|#
+(defun make-index-key ()
+  (make-sequence 'list 3))
 
 (defun make-fields-values-setter (fields)
-  "Creates a function which destructively modifies
-the specified fields in the index key passed to it
-as a parameter"
+  "Returns a function accepting index key
+as the first parameter, field values as the rest parameters,
+and destructively modifies the index key by setting the field
+values in it. Names of fields to modify in the index key,
+and their order is specified by FIELDS."
   (let ((index-key-setters (list :lisp #'(lambda (index-key lisp)
                                            (setf (first index-key) lisp))
                                  :lib-world #'(lambda (index-key lib-world)
@@ -1229,6 +1227,10 @@ as a parameter"
             index-key)))))
 
 (defun make-fields-values-getter (fields)
+  "Returns a function accepting and index key as a parameter,
+and returning a list of field values from that index key.
+The field names to retrieve, and their order in the
+returned list is specified by FIELDS."
   (let ((index-key-getters (list :lisp #'first
                                  :lib-world #'second
                                  :libname #'third)))
@@ -1241,34 +1243,102 @@ as a parameter"
                         (funcall getter index-key))
                     getters))))))
 
-(defun calc-rows-and-cols (joined-index rows-fields cols-fields)
-  (let ((rows-fields-getter (make-fields-values-getter rows-fields))
-        (rows-fields-setter (make-fields-values-setter rows-fields))
-        (rows (make-hash-table :test #'equal))
-        (cols-fields-getter (make-fields-values-getter cols-fields))
-        (cols-fields-setter (make-fields-values-setter cols-fields))
-        (cols (make-hash-table :test #'equal)))
-    (maphash #'(lambda (index-key index-value)
-                 (declare (ignore index-value))
-                 (setf (gethash (funcall rows-fields-getter index-key)
-                                rows)
-                       t)
-                 (setf (gethash (funcall cols-fields-getter index-key)
-                                cols)
+;; Lets introduce a notion of row and columns addresses.
+;; An address is a list with values for the fields
+;; we put into the row or column header.
+;;
+;; For example, if we want a privot report with lisp
+;; implementations as columns, and lisp-world and
+;; library name as rows, then we may have column
+;; addresses like
+;;   ("sbcl-win-1.0.55")
+;;   ("clisp-win-2.49")
+;;   ("abcl-1.1")
+;; and row addresses like
+;;   ("quickisp 2012-02-03" "alexandria")
+;;   ("quickisp 2012-02-03" "babel")
+;;   ("quickisp 2012-03-03" "cl-json")
+;;
+;; Order of values in the address depends on the order
+;; of grouping we chosen for pivot reports. In the above
+;; row addresses lib-worlds are larger groups, and for
+;; every lib-world we enumerate library names. In another
+;; pivot report we may want other way around: to first group
+;; data by libnames, and then subdivede these groups by
+;; lib-worlds. In this case row addresses would be
+;;   ("alexandria" "quickisp 2011-12-07" )
+;;   ("alexandria" "quickisp 2012-01-03" )
+;;   ("alexandria" "quickisp 2012-02-03" )
+;;   ("babel" "quickisp 2011-12-07" )
+;;   ("babel" "quickisp 2012-02-03" )
+;;
+
+(defun distinct-addresses (joined-index address-fields)
+  (let ((distinct (make-hash-table :test #'equal))
+        (fields-getter (make-fields-values-getter address-fields)))
+    (maphash #'(lambda (index-key unused-index-value)
+                 (declare (ignore unused-index-value))
+                 (setf (gethash (funcall fields-getter index-key)
+                                distinct)
                        t))
              joined-index)
-    (values (hash-table-keys rows)
-            (hash-table-keys cols)
-            #'(lambda (index-key row-addr col-addr)
-                (funcall rows-fields-setter index-key row-addr)
-                (funcall cols-fields-setter index-key col-addr)))))
+    (hash-table-keys distinct)))
 
+;; Take into account the specifics of HTML tables - the
+;; headers which group several rows or columns, will
+;; have a single table cell with rowspan or colspan atribute
+;; set to group size.
+;;
+;; Example HTML for a pivot report with row headers
+;; containing lib-world and libname (in that order),
+;; and column headers containing lisp implementation name.
+;; Pay attantion to the rowspan attribute:
+
+#|
+
+<tr> <th>                                </th> <th>          </th> <th>sbcl-win-1.0.55</th> <th>clisp-linux-2.49</th> </tr>
+<tr> <th rowspan="3">quicklisp 2012-01-03</th> <th>alexandria</th> <td>    data...    </td> <td>    data...     </td> </tr>
+<tr>                                           <th>babel     </th> <td>    data...    </td> <td>    data...     </td> </tr>
+<tr>                                           <th>cl-json   </th> <td>    data...    </td> <td>    data...     </td> </tr>
+<tr> <th rowspan="2">quicklisp 2012-02-07</th> <th>alexandria</th> <td>    data...    </td> <td>    data...     </td> </tr>
+<tr>                                           <th>cl-json   </th> <td>    data...    </td> <td>    data...     </td> </tr>
+
+|#
+
+;; If we put more thatn one field into the columns, we will
+;; have similar situation with colspans. When printing table
+;; row by row, cell by cell, we need to know what will be rowspan
+;; or colspan for particular <th> cell, and whether we already
+;; printed a <th> element for the current group. Lets calculate it.
+;;
+;; Subaddress is a prefix of row or column address.
+;; It represents a some level of pivot groupping.
+(defun subaddrs (row-address)
+  (nreverse (maplist #'reverse (reverse row-address))))
+
+(assert (equal '((1) (1 2) (1 2 3))
+               (subaddrs '(1 2 3))))
+
+;; For every subaddress (group) we calculate a
+;; it's span (number of rows/columns in the group),
+;; and store a flag, whether we already printed
+;; the <th>.
 (defstruct (header-print-helper :conc-name)
   (span 0 :type fixnum)
   (printed nil))
 
-(defun subaddrs (row-address)
-  (nreverse (maplist #'reverse (reverse row-address))))
+;; Here is the main calculation. Returns
+;; a hash table mapping subaddresses to header-print-helper objects.
+;; For the above example of pivot HTML table, the hashtable
+;; for rows would be:
+;;
+;;  ("quicklisp 2012-01-03")              -> #S(span 3 printed nil)
+;;  ("quicklisp 2012-01-03" "alexandria") -> #S(span 1 printed nil)
+;;  ("quicklisp 2012-01-03" "cl-json")    -> #S(span 1 printed nil)
+;;  ("quicklisp 2012-01-03" "cl-json")    -> #S(span 1 printed nil)
+;;  ("quicklisp 2012-02-07")              -> #S(span 2 printed nil)
+;;  ("quicklisp 2012-02-07" "alexandria") -> #S(span 1 printed nil)
+;;  ("quicklisp 2012-02-07" "cl-json")    -> #S(span 1 printed nil)
 
 (defun calc-spans (row-or-col-addrs)
   (let ((helpers (make-hash-table :test #'equal)))
@@ -1280,6 +1350,7 @@ as a parameter"
           (incf (span helper)))))
     helpers))
 
+;; This is how we print data in table data cells.
 (defun format-lib-results (out lib-results)
   (dolist (lib-result lib-results)
     (let ((status (aggregated-status (getf lib-result :status))))
@@ -1299,9 +1370,20 @@ as a parameter"
                 (string-downcase (car (last subaddr))))
         (setf (printed helper) t)))))
 
+;; Two alternative ways of column headers.
+;;
+;; This is used when we have enought horizontal space,
+;; just a usual <th> element.
 (defun print-usual-col-header (colspan text out)
   (format out "<th colspan=\"~A\">~A</th>" colspan text))
 
+;; When we need to save horizontal space, we print
+;; column headers rotages, so the headers do not
+;; requier more than 1ex from columns width
+;; (the data cell may make the column wider,
+;; than 1ex of course, but the headers never
+;; increase the column width more than needed
+;; for the data cells).
 (defun print-rotated-col-header (colspan text out)
   ;; Calculate the height so that rotated text fits
   ;; into it: multiply the length of the text to
@@ -1313,7 +1395,7 @@ as a parameter"
     (format out "<th colspan=\"~A\" class=\"rotated-header\" style=\"height: ~Aex\"><div>~A</div></th>"
             colspan css-height text)))
 
-(defun print-table-headers (row-field-count col-field-count cols out)
+(defun print-col-headers (row-field-count col-field-count cols out)
   (let ((col-count (length cols))
         (col-spans (calc-spans cols))
         (header-row-count col-field-count))
@@ -1329,8 +1411,16 @@ as a parameter"
           (when (not (printed helper))
             (let ((colspan (span helper))
                   (text (string-downcase (car (last cell-addr)))))
+              ;; Heuristic to determine if we need to rotate
+              ;; the column headers:
+              ;; more than 7 columns, and it is the last
+              ;; row of column header (BTW, this is the place where
+              ;; the code might need to be adjusted if
+              ;; we start using it for pivot reports with more than
+              ;; the 3 fields - :lib-world, :libname, :lisp -
+              ;; in the headers; actually, it's not perfect even
+              ;; now).
               (if (and (> col-count 7)
-                       ;; and the last header row:
                        (= header-row-num (1- header-row-count)))
                   (print-rotated-col-header colspan text out)
                   (print-usual-col-header colspan text out)))
@@ -1346,32 +1436,36 @@ as a parameter"
   (assert (= (length col-fields) (length col-fields-sort-predicates)))
 
   (princ "<table border=\"1\" class=test-table>" out)
-  (let (rows
-        cols
-        index-key-setter
-        (row-comparator #'(lambda (rowa rowb)
-                            (list< row-fields-sort-predicates
-                                   rowa rowb)))
-        (col-comparator #'(lambda (cola colb)
-                            (list< col-fields-sort-predicates
-                                   cola colb))))
 
-    (setf (values rows cols index-key-setter)
-          (calc-rows-and-cols joined-index row-fields col-fields))
+  (let* ((row-comparator #'(lambda (rowa rowb)
+                             (list< row-fields-sort-predicates
+                                    rowa rowb)))
+         (col-comparator #'(lambda (cola colb)
+                             (list< col-fields-sort-predicates
+                                    cola colb)))
+         (rows (sort (distinct-addresses joined-index row-fields)
+                     row-comparator))
+         (row-spans (calc-spans rows))
+         (cols (sort (distinct-addresses joined-index col-fields)
+                     col-comparator))
+         ;; this index key will be destructively modified
+         ;; when we need to access the data, to avoid
+         ;; consing of creation of new indexe every time
+         ;; (is it a prelimenary optimization?)
+         (index-key (make-index-key))
+         (rows-fields-setter (make-fields-values-setter row-fields))
+         (cols-fields-setter (make-fields-values-setter col-fields)))
 
-    (setf rows (sort rows row-comparator)
-          cols (sort cols col-comparator))
-
-    (print-table-headers (length row-fields) (length col-fields) cols out)
-    (let ((row-spans (calc-spans rows))
-          (index-key (make-sequence 'list (+ (length row-fields)
-                                             (length col-fields)))))
+    (print-col-headers (length row-fields) (length col-fields) cols out)
+    (flet ((test-results-by-row-col (row-addr col-addr)
+             (funcall rows-fields-setter index-key row-addr)
+             (funcall cols-fields-setter index-key col-addr)
+             (gethash index-key joined-index)))
       (dolist (row rows)
         (princ "<tr>" out)
         (print-row-header row row-spans out)
         (dolist (col cols)
-          (funcall index-key-setter index-key row col)
-          (let ((lib-results (gethash index-key joined-index)))
+          (let ((lib-results (test-results-by-row-col row col)))
             (princ "<td>" out)
             (format-lib-results out lib-results)
             (princ "</td>" out)))
