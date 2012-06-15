f1fb2877e0782d7606d592fcdcc773aa8ac3caee
Anton Vodonosov <avodonosov@yandex.ru>
2012-06-15 22:59:31 +0300

    Added forgotten fake-blobstore.lisp)

---
+++ agent/fake-blobstore.lisp
@@ -0,0 +1,26 @@
+;;;; -*- Mode: LISP; Syntax: COMMON-LISP; indent-tabs-mode: nil; coding: utf-8; show-trailing-whitespace: t -*-
+;;;; Copyright (C) 2011 Anton Vodonosov (avodonosov@yandex.ru)
+;;;; See LICENSE for details.
+
+;;;
+;;; Fake implementation of test-grid-blobstore API;
+;;; may be used for testing when we don't want really
+;;; upload test results to server
+;;;
+
+(in-package #:test-grid-agent)
+
+(defclass fake-blobstore () ())
+
+(defmethod test-grid-blobstore:submit-files ((blobstore fake-blobstore) id-pathname-alist)
+  (log:info "fake-blobstore.submit-files called")  
+  ;; return random fake blob IDs for every file
+  (let ((random-state (make-random-state t)))
+    (mapcar (lambda (id-pathname-cons)
+              (cons (car id-pathname-cons)
+                    (format nil "fake-~36,10,'0r" (random most-positive-fixnum random-state))))
+            id-pathname-alist)))
+
+(defmethod test-grid-blobstore:submit-run-info ((blobstore fake-blobstore) run-info)
+  (declare (ignore run-info))
+  (log:info "fake-blobstore.submit-run-info called"))
