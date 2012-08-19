8349280ea91d61386a0ef70b8df8651c72810b66
Anton Vodonosov <avodonosov@yandex.ru>
2012-08-19 20:14:30 +0300

    Simplified CSV export function.

--- reporting/csv.lisp
+++ reporting/csv.lisp
@@ -4,16 +4,15 @@
 
 (in-package #:test-grid-reporting)
 
-(defun export-to-csv (out &optional (db test-grid-data::*db*))
+(defun export-to-csv (out db)
   (format out "Lib World,Lisp,Runner,LibName,Status,TestDuration~%")
-  (dolist (run (getf db :runs))
-    (let ((run-descr (test-grid-data::run-descr run)))
-      (dolist (lib-result (test-grid-data::run-results run))
-        (format out "~a,~a,~a,~a,~a,~a~%"
-                (getf run-descr :lib-world)
-                (getf run-descr :lisp)
-                (getf (getf run-descr :contact) :email)
-                (string-downcase (getf lib-result :libname))
-                (aggregated-status (getf lib-result :status))
-                (float (getf lib-result :test-duration)))))))
+  (do-results (result db)
+    (format out "~a,~a,~a,~a,~a,~a~%"
+            (lib-world result)
+            (lisp result)
+            (contact-email result)
+            (string-downcase (libname result))
+            (aggregated-status (status result))
+            (float (test-duration result)))))
+
 
