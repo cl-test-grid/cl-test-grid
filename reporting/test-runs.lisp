58e3cf147b0ad6eb9207d1416ca96f9a922130b5
Anton Vodonosov <avodonosov@yandex.ru>
2012-08-26 05:05:06 +0300

    Hardcode default path to the local test-runs directory we use to generate URI if log links, when no blobstore key is assigned to the test result. Hardcoded because now it can not be determined automatically.

--- reporting/test-runs.lisp
+++ reporting/test-runs.lisp
@@ -31,10 +31,12 @@
   (format nil "~A/blob?key=~A"
           "http://cl-test-grid.appspot.com" blob-key))
 
+(defparameter *local-test-runs-dir* #P"C:\\Users\\anton\\projects\\cl-test-grid2-work-dir2\\agent\\test-runs\\")
+
 (defun lib-log-local-uri (joined-lib-result)
   (format nil "file://~A~A"
           (test-grid-agent::run-directory (test-grid-data::run-descr (test-run joined-lib-result))
-                                          (test-grid-agent::test-output-base-dir))
+                                          *local-test-runs-dir*)
           (string-downcase (getf (lib-result joined-lib-result) :libname))))
 
 (defun no-blob-key-js-alert (&rest unused-args)
