984a0d4806be36239adfb0ffab5eb53575f3d0f2
Anton Vodonosov <avodonosov@yandex.ru>
2012-10-01 05:00:59 +0300

    reporting/durations.lisp - print the durations sorted by lisp implementation identifier.

--- reporting/durations.lisp
+++ reporting/durations.lisp
@@ -5,14 +5,20 @@
 (in-package #:test-grid-reporting)
 
 (defun print-test-run-durations (lib-world)
-  (dolist (run (getf (test-grid-data::read-db)
-                     :runs))
-    (let ((descr (test-grid-data::run-descr run)))
-      (when (string= lib-world (getf descr :lib-world))
-        (format t "~A ~A~%" 
-                (getf descr :lisp)
-                (float (/ (getf descr :run-duration)
-                          60)))))))
+  (let ((duration-alist '()))
+    (dolist (run (getf (test-grid-data::read-db)
+                       :runs))
+      (let ((descr (test-grid-data::run-descr run)))
+        (when (string= lib-world (getf descr :lib-world))
+          (pusn (cons (getf descr :lisp)
+                      (getf descr :run-duration))
+                duration-alist))))
+    (setf duration-alist (sort duration-alist #'string< :key #'car))
+    (dolist (pair duration-alist)
+      (format t "~A ~A~%"
+              (car pair)
+              (float (/ (cdr pair)
+                        60))))))
 
 ;; usage:
-;; (print-test-run-durations "quicklisp 2012-07-03")
+ (print-test-run-durations "quicklisp 2012-09-09")
