09b23e2b12e7ba5fe1eab05372776ff0bd30473f
Anton Vodonosov <avodonosov@yandex.ru>
2013-02-13 10:08:33 +0300

    Working on CL-implemented server, to run it on Heroku

--- heroku-compile.lisp
+++ heroku-compile.lisp
@@ -4,6 +4,9 @@
 (require 'asdf)
 (asdf:disable-output-translations)
 (require-quicklisp)
+(let* ((this-file (load-time-value (or *load-truename* #.*compile-file-pathname*)))
+       (this-file-dir (make-pathname :directory (pathname-directory this-file))))
+  (push this-file-dir asdf:*central-registry*))
 (ql:quickload :test-grid-server)
 (print ">>> Done building system")
 
