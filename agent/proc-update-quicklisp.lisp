f9e56e9d798c420e3199523ad28902c760e6d732
Anton Vodonosov <avodonosov@yandex.ru>
2014-05-01 16:42:44 +0300

    Handle ql-dist:badly-sized-local-archive during quicklisp update

--- agent/proc-update-quicklisp.lisp
+++ agent/proc-update-quicklisp.lisp
@@ -31,36 +31,55 @@
 (defmacro fncall (funname &rest args)
   `(funcall (read-from-string ,funname) ,@args))
 
+(defun restarting-downloads-impl (body-fn)
+  (let ((restart-counters (make-hash-table :test #'equal)))
+    (handler-bind
+        ((error (lambda (e)
+                  (when (typep e (read-from-string "ql-dist:badly-sized-local-archive"))
+                    (let* ((release (fncall "ql-dist::invalid-local-archive-release" e))
+                           (retry-count (incf (gethash release
+                                                       restart-counters
+                                                       0))))
+                      (when (<= retry-count 3)
+                        (format t "~A download retry #~A~%" release retry-count)
+                        (invoke-restart (read-from-string "ql-dist:delete-and-retry"))))))))
+      (funcall body-fn))))
+
+(defmacro restarting-downloads (&body body)
+  `(restarting-downloads-impl (lambda () ,@body)))
+
 (defun do-quicklisp-update (install-dir)
-  (install-quicklisp install-dir)
-  (fncall "quicklisp:update-client" :prompt nil)
+  (restarting-downloads
 
-  (flet ((version-string (dist)
-           ;; find a fresh DIST object in case it is stale,
-           ;; because QL:UPDATE-DIST only changes data on file system
-           ;; and does not update the DIST object
-           (let ((dist (fncall "ql-dist:dist" (fncall "ql-dist:name" dist))))
-             (format nil "~A ~A"
-                     (fncall "ql-dist:name" dist)
-                     (fncall "ql-dist:version" dist)))))
+    (install-quicklisp install-dir)
+    (fncall "quicklisp:update-client" :prompt nil)
 
-    ;; Update and use the "quicklisp" dist:
-    (let ((qlalpha (fncall "ql-dist:find-dist" "qlalpha")))
-      (when qlalpha (fncall "ql-dist:disable" qlalpha)))
-    (let ((dist (fncall "ql-dist:dist" "quicklisp")))
-      (fncall "ql-dist:enable" dist)
-      (fncall "ql:update-dist" dist :prompt nil)
-      (version-string dist))
-    ;; or, if we need to install particular quicklisp version:
-    ;;(version-string (fncall "ql-dist:install-dist" "http://beta.quicklisp.org/dist/quicklisp/2012-08-11/distinfo.txt" :replace t :prompt nil))
+    (flet ((version-string (dist)
+             ;; find a fresh DIST object in case it is stale,
+             ;; because QL:UPDATE-DIST only changes data on file system
+             ;; and does not update the DIST object
+             (let ((dist (fncall "ql-dist:dist" (fncall "ql-dist:name" dist))))
+               (format nil "~A ~A"
+                       (fncall "ql-dist:name" dist)
+                       (fncall "ql-dist:version" dist)))))
 
-    ;; Update and use the "qlalpha" dist:
-    ;;(fncall "ql-dist:disable" (fncall "ql-dist:dist" "quicklisp"))
-    ;;(let ((dist (fncall "ql-dist:install-dist" "http://alpha.quicklisp.org/dist/qlalpha.txt" :prompt nil :replace t)))
-    ;;  (fncall "ql-dist:enable" dist)
-    ;;  (fncall "ql:update-dist" dist :prompt nil)
-    ;;  (version-string dist))
-    ))
+      ;; Update and use the "quicklisp" dist:
+      (let ((qlalpha (fncall "ql-dist:find-dist" "qlalpha")))
+        (when qlalpha (fncall "ql-dist:disable" qlalpha)))
+      (let ((dist (fncall "ql-dist:dist" "quicklisp")))
+        (fncall "ql-dist:enable" dist)
+        (fncall "ql:update-dist" dist :prompt nil)
+        (version-string dist))
+      ;; or, if we need to install particular quicklisp version:
+      ;;(version-string (fncall "ql-dist:install-dist" "http://beta.quicklisp.org/dist/quicklisp/2012-08-11/distinfo.txt" :replace t :prompt nil))
+
+      ;; Update and use the "qlalpha" dist:
+      ;;(fncall "ql-dist:disable" (fncall "ql-dist:dist" "quicklisp"))
+      ;;(let ((dist (fncall "ql-dist:install-dist" "http://alpha.quicklisp.org/dist/qlalpha.txt" :prompt nil :replace t)))
+      ;;  (fncall "ql-dist:enable" dist)
+      ;;  (fncall "ql:update-dist" dist :prompt nil)
+      ;;  (version-string dist))
+      )))
 
 (defun update-quicklisp (install-dir log-file)
   (labels ((fmt-time (universal-time &optional destination)
